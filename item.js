d9d272e332b1498710f9187bf0e030159e613056
keke7744 <41632496+keke7744@users.noreply.github.com>
2019-06-28 12:44:32 +0200

    Add files via upload

---
+++ item.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg"
+
+const Item = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <img src={logo} alt=""/>
+                </li>
+            </ul>
+        </div>
+        )
+}
+ 
+export default Item
---
+++ itemlist.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Item from './item.js'
+
+
+function ListItem(props) {
+  
+  return (
+
+
+
+<div>
+<Item />
+<Item />
+<Item />
+<Item />
+
+</div>
+
+
+    
+ 
+  );
+}
+
+export default ListItem;
